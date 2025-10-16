a1ed2eddf055d76da2a17e877c2cfe6e197d78ea
leftii-dev <austin@austinbarnes.dev>
2025-10-16 13:01:30 -0700

    Complete NavBar desktop view Complete mobile view Add menu expand/collapse Add useRef for close by clicking off Convert logo/logo_text to plain svg (inkscape svg causes errors)

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 export default function AuthButton() {
     const { data: session, status } = useSession();
 
+
     const handleSignOut = async () => {
         try {
             await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/v1/auth/logout`, {
@@ -20,16 +21,24 @@ export default function AuthButton() {
         await signOut({ callbackUrl: '/login' });
     };
 
-    if (status === "loading") {
-        return <button disabled style={{ opacity: 0.5 }}>Loading...</button>;
+    if(status === "loading") {
+        // Same classes used to prevent layout shift while checking session status
+        return (
+            <div className="flex justify-end items-end gap-3 md:mb-4">
+                <div className={`md:h-12 md:w-52 bg-gray-200 rounded-md animate-pulse`}></div>
+            </div>
+        )
     }
 
     if (session) {
         return (
-            <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-                <span>Welcome, {session.user?.name || session.user?.email}</span>
+            <div className={`flex flex-col w-full md:flex-row md:justify-end md:items-end md:gap-3 md:whitespace-nowrap md:mb-4`}>
+                <span className={`hidden font-inter text-lg md:pt-2 md:block`}>Welcome, {session.user?.name?.split(' ')[0] || session.user?.email}!</span>
                 {/* The button now calls our custom handler */}
-                <button onClick={handleSignOut}>
+                <button
+                    className={`font-inter px-2 py-2 bg-blue-400 rounded md:rounded-md md:py-1 text-white md:whitespace-nowrap md:hover:bg-blue-300 hover:cursor-pointer`}
+                    onClick={handleSignOut}
+                >
                     Sign Out
                 </button>
             </div>
@@ -37,8 +46,23 @@ export default function AuthButton() {
     }
 
     return (
-        <Link href="/login">
-            <button>Sign In</button>
-        </Link>
+        <div className={`flex flex-col w-full items-center gap-y-2 md:flex-row md:justify-end md:items-end md:gap-3 md:mb-4`}>
+            <Link href="/login" className={`w-full`}>
+                <button
+                    className={`font-inter px-2 py-2 w-full text-white bg-brand-primary rounded-md md:border-2 md:bg-white md:border-brand-primary md:rounded-xl md:py-1 
+                                md:text-black md:hover:border-white md:hover:text-white 
+                                md:hover:bg-brand-primary md:hover:cursor-pointer`}>
+                    Login
+                </button>
+            </Link>
+            <Link href="/register" className={`w-full`}>
+                <button
+                    className={`font-inter w-full px-2 py-2 md:border-2 md:border-black md:bg-black md:text-white md:whitespace-nowrap md:rounded-xl md:py-1
+                                 md:hover:opacity-75 md:hover:cursor-pointer`}
+                >
+                    Sign Up
+                </button>
+            </Link>
+        </div>
     );
 }
