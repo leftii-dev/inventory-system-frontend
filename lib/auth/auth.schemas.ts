b05a6072e1f162ff61d6a509608fbcfd33a81dc5
leftii-dev <austin@austinbarnes.dev>
2025-10-18 08:53:05 -0700

    Add zod for validation. Add RegisterUserSchema for registration validation.

---
+++ lib/auth/auth.schemas.ts
@@ -0,0 +1,13 @@
+import {z} from 'zod';
+
+export const RegisterUserSchema = z.object({
+    email: z.email(),
+    name: z.string().min(2).max(50),
+    password: z.string()
+        .min(8, {error: "Password must be at least 8 characters long"})
+        .max(128, {error: "Password must be at most 128 characters long"})
+        .regex(/[a-z]/, {message: "Password must contain at least one lowercase letter"})
+        .regex(/[A-Z]/, {message: "Password must contain at least one uppercase letter"})
+        .regex(/[0-9]/, {message: "Password must contain at least one number"})
+        .regex(/[^a-zA-Z0-9]/, {message: "Password must contain at least one special character"})
+});
