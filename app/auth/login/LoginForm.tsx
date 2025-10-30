28ea4b3a3e6c7e042026ead6e85d1238e0ddc75b
leftii-dev <austin@austinbarnes.dev>
2025-10-29 22:42:42 -0700

    Add render child to LoginForm

--- app/auth/login/LoginForm.tsx
+++ app/auth/login/LoginForm.tsx
@@ -5,8 +5,9 @@ import FormCard from "@/components/form/FormCard";
 import {ActionResult} from "@/lib/utils/api.actions";
 import {LoginUserSchema} from "@/lib/auth/auth.schemas";
 import {z} from "zod";
-
-type LoginActionResult = ActionResult<typeof LoginUserSchema>
+import {loginAction} from "@/lib/auth/auth.actions";
+import SubmitButton from "@/components/SubmitButton";
+import FormInput from "@/components/form/FormInput";
 
 export default function LoginForm({
     initialData
@@ -15,17 +16,39 @@ export default function LoginForm({
 }) {
 
     return (
-        <FormCard<LoginActionResult>
+        <FormCard<ActionResult<typeof LoginUserSchema>>
             action={loginAction}
             initialState={{
                 success: true,
                 data: initialData,
             }}
         >
-            {(state) => (
-                <div>
-                </div>
-            )}
+            {(state) => {
+                if(!state.success) {
+                    state.errors = {...state.errors, general: 'Invalid email or password'}
+                }
+                    return(
+                        <div>
+                            <FormInput
+                                key={`email-${state.success}`}
+                                label={'Email:'}
+                                name={'email'}
+                                type={'text'}
+                                required={true}
+                            />
+                            <FormInput
+                                label={'Password:'}
+                                name={'password'}
+                                type={'password'}
+                                required={true}
+                            />
+
+                            <SubmitButton>
+                                Login
+                            </SubmitButton>
+                        </div>
+                    )
+            }}
         </FormCard>
-    )
+    );
 }
