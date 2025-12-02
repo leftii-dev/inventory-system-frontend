e719793dcd6e414e3de32ad1cdf15ce76d6bde9f
leftii-dev <austin@austinbarnes.dev>
2025-12-01 17:07:57 -0800

    Add actions for PurchaseOrders Vendors and Status

--- lib/inventory/inventory.actions.ts
+++ lib/inventory/inventory.actions.ts
@@ -1,12 +1,45 @@
 'use server';
 
 import {ActionResult, apiAction} from "@/lib/utils/api.actions";
-import {PurchaseOrderResponse} from "@/lib/inventory/inventory.types";
+import {PurchaseOrderResponse, StatusResponse, VendorResponse} from "@/lib/inventory/inventory.types";
 
 export async function getPurchaseOrders(): Promise<ActionResult<PurchaseOrderResponse[]>> {
     return await apiAction<PurchaseOrderResponse[]>({
         endpoint: "/purchase-orders",
         method: 'GET',
         requireAuth: true,
+    })
+}
+
+export async function getPurchaseOrder(id: string): Promise<ActionResult<PurchaseOrderResponse>> {
+    return await apiAction<PurchaseOrderResponse>({
+        endpoint: `/purchase-orders/${id}`,
+        method: 'GET',
+        requireAuth: true,
+    })
+}
+
+export async function getVendors(): Promise<ActionResult<VendorResponse[]>> {
+    return await apiAction<VendorResponse[]>({
+        endpoint: "/vendors",
+        method: 'GET',
+        requireAuth: true,
+    })
+}
+
+export async function getStatuses(): Promise<ActionResult<StatusResponse[]>> {
+    return await apiAction<StatusResponse[]>({
+        endpoint: "/status",
+        method: 'GET',
+        requireAuth: true,
+    })
+}
+
+export async function createPurchaseOrder(
+    prevState: ActionResult<PurchaseOrderResponse>,
+    formData: FormData
+): Promise<ActionResult<PurchaseOrderResponse>> {
+    return await apiAction<PurchaseOrderResponse>({
+        schema:
     })
 }
